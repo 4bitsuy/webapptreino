f29fdb5f7254a79b1bd29bec811f48d6b614722f
rsinibaldi <ro.sinibaldi@gmail.com>
2018-02-16 10:18:59 -0300

    creación de BD [migrations]

--- treino.com/database/migrations/2018_02_13_131653_alta_tbl_rol.php
+++ treino.com/database/migrations/2018_02_13_131653_alta_tbl_rol.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AltaTblRol extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('usuario', function (Blueprint $table) {
+        Schema::create('rol', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('clave');
-            $table->string('email')->unique();
-            $table->string('documento');
-            $table->rememberToken();
+            $table->string('descripcion');
+            $table->string('nombre');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuario');
+        Schema::dropIfExists('rol');
     }
 }
