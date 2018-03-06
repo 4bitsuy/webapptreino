b81215c5e19d99abfdd287837bb11ace16cb82a5
rsinibaldi <ro.sinibaldi@gmail.com>
2018-03-05 23:02:25 -0300

    controller docenten¡, alumno y modificacion en campus

--- treino.com/app/Http/Controllers/Campus/CampusController.php
+++ treino.com/app/Http/Controllers/Campus/CampusController.php
@@ -67,6 +67,7 @@ class CampusController extends Controller
     private function setSessionUser($usuario, $rol){
       session(['usuId' => $usuario->id]);
       session(['usuMail' => $usuario->email]);
+      session(['usuDocu' => $usuario->documento]);
       session(['usuName' => $usuario->name]);
       session(['usuRolId' => $rol->id]);
       session(['usuRol' => $rol->nombre]);
