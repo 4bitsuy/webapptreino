105fa17bd1ac0818321ae4878134ef1716460e56
4bitsuy <35241126+4bitsuy@users.noreply.github.com>
2018-03-20 10:59:11 -0300

    funcion getPorcentCurso y cambios.

--- treino.com/app/Http/Controllers/Campus/AlumnoController.php
+++ treino.com/app/Http/Controllers/Campus/AlumnoController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\CollectionCollection;
 use Illuminate\Support\Collection;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use App\Alumno;
 use App\Persona;
 use App\Cursa;
@@ -61,14 +62,16 @@ class AlumnoController extends Controller{
   private function getCursos($alu_id){
 
     $Cursos = Cursa::where('alu_id',$alu_id)->get();
-    $RelTemaAlu  = RelTemaAlu::where('alu_id',$alu_id)->first();
-
+    $TemaId = RelTemaAlu::where('alu_id',$alu_id)->pluck('tema_id')->first();
+    
     $datos_cursos = [];
 
     foreach ($Cursos as $Curso) {
         $Modulo = Modulo::where('modu_id',$Curso->modu_id)->first();
         $Grado  = Grado::where('gra_id',$Curso->gra_id)->first();
-        $Tema  = Tema::where('tema_id',$Curso->tema_id)->first();
+        $Tema  = Tema::where('tema_id',$TemaId)->first();
+    
+        $PorcentajeCurso =  $this->getPorcentCurso($Tema);
 
         $item_datos_cursos = [];
         $item_datos_cursos =
@@ -87,7 +90,9 @@ class AlumnoController extends Controller{
             'cur_estado' => $Curso->cur_estado,
             'tema_nombre' => $Tema->tema_nombre,
             'tema_descripcion' => $Tema->tema_descripcion,
-            'tema_es_cur_corto' => $Tema->tema_es_cur_corto
+            'tema_es_cur_corto' => $Tema->tema_es_cur_corto,
+            'tema_fch_ini' => $Tema->tema_fch_ini,
+            'tema_fch_fin' => $Tema->tema_fch_fin
         ];// Fin array
         $datos_cursos = array_add($datos_cursos,$Curso->cur_id,$item_datos_cursos); //Agrego una coleccion de arrays
 
@@ -96,7 +101,19 @@ class AlumnoController extends Controller{
 
     return $datos_cursos;
 
-  }
+  }//Fin Function getCursos
+  
+  
+  private function getPorcentCurso($Tema){
+    
+    if (isset($Tema->tema_fch_ini) && isset($Tema->tema_fch_fin)){
+      $PorcentajeCurso = $Tema->tema_fch_ini->diffInDays($Tema->tema_fch_fin);
+    }else{
+      $PorcentajeCurso = 0;
+    }
+    
+    return $PorcentajeCurso;
 
-
+  }//Fin Function getPorcentCurso
+  
 }
